ad2d884e6608b2fe783da635fb77a69a8719fd6d
RaniQK <71139588+RaniQK@users.noreply.github.com>
2021-10-31 16:30:43 -0400

    Add files via upload

---
+++ Scipt.java
@@ -0,0 +1,159 @@
+int caption;
+int bubble;
+Total page 16;
+
+//210 words per page. 6 panels: 35 words per. 9 panels: 20-25 words per.
+
+Section 1 {
+	Page 1 { //telling reprequisite. whole page panel
+		caption: In ancient Egyptian mythology, cats played a very important role. It is said that cats ruled humans a long time ago. 
+		caption: They were cunning, cruel and very smart. They drove humans as slaves.
+		caption: Until the end of the dog ’s life When they appeared, they drove the cat away and turned the cat from a ruler into a human pet. So the dog was regarded by the Egyptians as the most important friend in life.
+		bubble: And the Egyptians believed that cats would bring death. //friend holds tea cup
+	}
+}
+
+Section 2 {
+	Page 2{ //telling reprequisite. whole page panel with crossing captions/bubbles
+		bubble: Does the Egyptian myth only involve cats? //I asked
+		bubble: Of course not. I am telling the story of an oriental cat today. //friend takes a sip
+
+		caption: The myth has it that if a creature collects seven wits in the world, it becomes immortal.
+				 cats too, of course - magical cats collect an extra tail every 20 yrs. Once they have 9 tails, they become immortal.
+		bubble: but that's not easy. for their 9th tail, they have to grant someone's wish, and wish granting takes away one tail. so its a loop.
+
+		bubble: but this is really just a myth. I heard that cat only helps the decendants of its first owner, and will grant ANY wish. 
+		bubble: still, it never got its 9th tail.
+	} //end page 2
+
+	Page 3.5 { //panel that leads to the 2nd half of this page
+		bubble: but I did hear some stories from my great grandmother last time I visited.
+				my hometown grows rich batches of crops. it was weird that none of the villagers kept dogs.
+				but cats were loved. they caught mice and never spread diseases.
+
+	} //end page 3.5
+}
+
+Section 3 { //story setting, general POV
+	Page 3.5: 
+		caption: I've heard about the eight-tailed cat. Legends has it that it's white and the size of a dog.
+				The villagers respected the cat.
+				After the owner of the cat died, it never returnd. The children of the owner suceeded in everything thereafter.
+	
+	Page 4: //girl's POV
+		caption: what a nasty weather //stormy 
+				there really shouldnt be any wolves...
+		bubble: but guess i am wrong //scared girl
+				HELP!!! //shouts in bubble language
+
+		// then there it was. a white cat. size of a lion with 8 tails
+		// the wolves ran away
+		//girl amazed
+
+	Page 5:
+		caption: facing the cat, i forgot that i was a human. a human that conquers and claims everything on earth as their own.
+		// cat bends down and looks at me soft
+		bubble: so i am the descendant, huh?
+				Thank you saving my life. can I pet you? //so beautiful - astonished face
+
+		//sun appears
+		// girl pets the cat. the fur of the cat is almost transparent.
+		//panel of girl's hand through the hair
+	Page 6:
+	    //the cat jumps away, looks at girl
+		bubble: you are waiting for my wish, arent you?
+				well, i dont know what i want at this point. how about i let you know when i do?
+		//the cat becomes a small regular cat
+		//girl happy face
+		//girl carries cat home
+
+	Page 7:
+		caption: i played with the cat everyday ever since. 
+		Adults from the village never keep children away from cats, and i had nothing else better to do.
+		//throws yarn ball at cat
+		//throws toys at cat
+		//cat looks displeased
+		bubble: huh, guess i was being disrepectful.
+
+	Page 8: 
+		bubble: how do you treat a magical cat anyways? //hand on chin, lazy look on face
+		//girl brings cat to school
+		//girl walks with cat afterschool
+		//girl shares food with cat
+
+	Page 9:
+		//evening
+		bubble: wheres my cat, father?
+		bubble: its on the lawn as it usually is when its not out with you
+		//finds cat on lawn
+		//cat meows impatiently
+		bubble: you really want to grant me that wish, huh?
+				you will lose a tail. you wont ever get 9 tails.
+				is that what you really want?
+				//silence
+
+	Page 10:
+		bubble: so you can grant any wish?
+		//cat looks at girl
+		bubble: well...
+				then, my wish is, I want YOU to get your nineth tail.
+				//girl looks down at cat smiling
+				//cat looks up at girl surprised
+				//cat looks up at girl happy
+		//cat standsup and walks near
+		//cat licks girls hand
+	Page 11:
+		//cats get nineth tail, lots of light
+		bubble: well, congratulations
+		//cat turns around and runs away
+
+		caption: the next day
+		bubble: i swear there was some weird light emitting from your house//tiedan
+		bubble: yeah no I dont know what youre talking about.5
+
+		//grandma
+		caption: ever since, things work out perfectly fine. I know the cat had been blessing me and my family.
+
+}
+
+Section 4 {
+	Page 12: 
+		caption: that was the story my great grandmother told about my grandma.
+		//g-gramma passes away
+		caption: she passed away due to old age some time after.
+				she was in her chair, and never woke up from her afternoon nap.
+				it was a red farewell, my family says.
+
+	Page 13: 
+		//i was near the grave
+		caption: meow 
+		//i look back surprised
+		//the cat was there, huge, 9 tails looking at me softly
+		caption: i wanted to wake my family, but i could not say a word. 
+		//it came near
+
+	Page 14: 
+		//the cat came to me and licked my hand
+		//the cat goes to grandma, licked her hand, and left
+		caption: i was unable to tell about this for quite a while.
+				I know people might laugh at me.
+	Page 15: 
+		bubble: I never saw the nine tailed cat ever since.
+				do you believe me?
+		bubble: of course I do.
+		bubble: well, that was quite a story to tell. Im glad you believe me.
+		//pats on shoulder
+		bubble: get some rest now
+		
+	Page 16:
+		//friend's cat comes in
+		bubble: I wonder if you are the cat,
+
+
+}
+
+
+
+
+
+
